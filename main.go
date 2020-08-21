53bde435e7fabed4caefb9f6291598bf85fcd983
Sean Robertson <serobertson@gmail.com>
2020-08-21 23:51:47 +0100

    create BuildEnvironment api

--- main.go
+++ main.go
@@ -75,6 +75,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "BuildJob")
 		os.Exit(1)
 	}
+	if err = (&controllers.BuildEnvironmentReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("BuildEnvironment"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "BuildEnvironment")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
